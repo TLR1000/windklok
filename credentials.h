1d09c1485367c5ccd4aabcd75aabca592f4223bf
TLR1000 <oostrijck@gmail.com>
2021-11-15 15:07:34 +0100

    Create credentials.h

---
+++ credentials.h
@@ -0,0 +1,12 @@
+#ifndef __CREDENTIALS_H__
+#define __CREDENTIALS_H__
+
+// Wifi parameters
+char ssid[] = "";
+char passphrase[] = "";
+char myhostname[] = "TEST-Windklok";
+
+//cloudmgqtt server config
+char windklokApiEndpoint[] = "https://";
+
+#endif
